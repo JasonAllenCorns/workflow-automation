c7eb60674c44d9e2d71d83f63b29849ad0e6ecf8
kostkams <matthias@kostkams.de>
2018-06-15 00:47:36 +0200

    Initial release

---
+++ test/test.js
@@ -0,0 +1,8 @@
+'use strict';
+var expect = require('chai').expect;
+var index = require('../dist/index.js');
+describe('Empty Test', () => {
+    it('should work', () => {
+        expect(true).to.equal(true);
+    });
+});
