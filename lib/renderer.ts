3fcc88258e95bd9c94d6a36c3419c7a964c74bfd
kostkams <matthias@kostkams.de>
2018-06-19 00:48:07 +0200

    bugfix: add unique array to the dataset

--- lib/renderer.ts
+++ lib/renderer.ts
@@ -10,7 +10,7 @@ export class Renderer {
     private initialized: boolean;
 
     public init(transitions: Transition[]) {
-        var nodeArray = Transition.getStatesFromTransitions(transitions);
+        var nodeArray = _.uniqWith(Transition.getStatesFromTransitions(transitions), (state:StateBase) => state.uuid);
         this.nodes = new vis.DataSet(_.map(nodeArray, (state: StateBase) => {
             return {id: state.uuid, label: state.name};
         }));
