39f5d6b12700e59bfbcd87d23b745d3e1cfd42c5
kostkams <matthias@kostkams.de>
2018-06-19 00:40:40 +0200

    use new version

--- example/SimpleWorkflow/src/app/app.component.ts
+++ example/SimpleWorkflow/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Workflow } from "workflow-automation/dist/workflow";
-import { Start } from "workflow-automation/dist/start";
-import { End } from "workflow-automation/dist/end";
-import { Transition } from "workflow-automation/dist/transition";
-import { SimpleState } from "workflow-automation/dist/simple_state";
-import { WorkflowState } from "workflow-automation/dist/workflowState";
+import { Workflow } from "workflow-automation/dist";
+import { Start } from "workflow-automation/dist";
+import { End } from "workflow-automation/dist";
+import { Transition } from "workflow-automation/dist";
+import { SimpleState } from "workflow-automation/dist";
+import { WorkflowState } from "workflow-automation/dist";
 import * as _ from 'lodash';
 import { StateBase } from "workflow-automation/dist/state_base";
 
@@ -32,22 +32,32 @@ export class AppComponent implements OnInit {
     var transition1 = new Transition('transition 1', start, state1);
     var transition2 = new Transition('transition 2', state1, state2);
     var transition3 = new Transition('transition 3', state2, end);
+    var transition4 = new Transition('transition 3', end, start);
 
-    this.workflow.transitions = [transition1, transition2, transition3];
+    this.workflow.transitions = [transition1, transition2, transition3, transition4];
     this.workflow.namespace = 'simple_workflow';
     this.workflow.init(this.workflowObject);
+    this.setCurrentState();
   }
 
   next() {
     this.workflow.next();
-    var currentStates = (<WorkflowState>this.workflowObject['simple_workflow']).currentStates;
-    if (currentStates != null && currentStates.length === 1) {
-      this.currentState = currentStates[0].name;
-    }
 
+    this.setCurrentState();
+    this.setHandledStates();
+  }
+
+  private setHandledStates() {
     var handledStates = (<WorkflowState>this.workflowObject['simple_workflow']).handledStates;
     if (handledStates != null && handledStates.length > 0) {
       this.finishedStates = _.map(handledStates, (item: StateBase) => item.name);
+    }
+  }
+
+  private setCurrentState() {
+    var currentStates = (<WorkflowState>this.workflowObject['simple_workflow']).currentStates;
+    if (currentStates != null && currentStates.length === 1) {
+      this.currentState = currentStates[0].name;
     }
   }
 }
